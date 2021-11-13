4fcd07d6ad075d055ce143fbe03ad821283b7f5f
SakshiArora2398 <71814862+SakshiArora2398@users.noreply.github.com>
2021-11-13 18:29:54 -0500

    Update index-photos.py

--- Lambda Functions/index-photos.py
+++ Lambda Functions/index-photos.py
@@ -1,33 +1,58 @@
 import json
 import boto3
-import time
 import data_index
+import time
 
-region='us-east-1' 
-client_s3= boto3.client('s3')
+s3_client = boto3.client('s3')
+region = 'us-east-1' 
 
 def lambda_handler(event, context):
     print(event)
-    labels=[]
+    label_list=[]
+    
     for record in event['Records']:
-        photos_bucket=record['s3']['bucket']['name']
-        name_of_file=record['s3']['object']['key']
+        PHOTO_BUCKET = record['s3']['bucket']['name']
+        FILE_NAME = record['s3']['object']['key']
+        response = s3_client.head_object(Bucket=PHOTO_BUCKET, Key=FILE_NAME)
+        custlabel=response['ResponseMetadata']['HTTPHeaders']['x-amz-meta-customlabels']
+        custlabellist=custlabel.split(',')
+        for cust in custlabellist:
+            label_list.append(cust)
 
-    print('reading image: {} from s3 bucket {}'.format(name_of_file, photos_bucket))
-    client_rek=boto3.client('rekognition')
-    response = client_rek.detect_labels(Image={'S3Object': {'Bucket':photos_bucket,'Name': name_of_file}},MaxLabels=12,MinConfidence=80,)
-    print('Detected labels for ' + name_of_file)
+
+    print('reading image: {} from s3 bucket {}'.format(FILE_NAME, PHOTO_BUCKET))
+    client = boto3.client('rekognition')
+    response = client.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': PHOTO_BUCKET,
+                'Name': FILE_NAME
+            }
+        },
+        MaxLabels=12,
+        MinConfidence=80,
+    )
+    
+    print('Detected labels for ' + FILE_NAME)
     for label in response['Labels']:
         print (label['Name'] + ' : ' + str(label['Confidence']))
-        labels.append(label['Name'].lower())
+        label_list.append(label['Name'].lower())
     
-    times=time.gmtime()
-    time_created=time.strftime("%Y-%m-%dT%H:%M:%S", times)
-    print('Image created at {}....'.format(time_created))
-    image_obj={'objectKey':name_of_file,'bucket':photos_bucket,'createdTimestamp':time_created,'labels':labels}
-    print(str(image_obj))
-    es=data_index.connect_to_elastic_search()
-    es.index(index="photos", doc_type="_doc", id=time_created, body=image_obj)
-    response=es.get(index="photos", doc_type="_doc", id=time_created,refresh=True)
+    ts = time.gmtime()
+    created_time = time.strftime("%Y-%m-%dT%H:%M:%S", ts)
+    print('Image created at {}....'.format(created_time))
+    
+    image_object = {
+        'objectKey':FILE_NAME,
+        'bucket':PHOTO_BUCKET,
+        'createdTimestamp':created_time,
+        'labels':label_list
+    }
+    print(str(image_object))
+    es = data_index.connect_to_elastic_search()
+    es.index(index="photos", doc_type="_doc", id=created_time, body=image_object)
+
+    response = es.get(index="photos", doc_type="_doc", id=created_time,refresh=True)
     print(response)
     return response
+

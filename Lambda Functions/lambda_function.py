302fa567badc3b52839495cedc9e9c9a01e8fb71
SakshiArora2398 <71814862+SakshiArora2398@users.noreply.github.com>
2021-11-23 03:40:19 -0500

    Update lambda_function.py

--- Lambda Functions/lambda_function.py
+++ Lambda Functions/lambda_function.py
@@ -6,8 +6,8 @@ import data_index
 def lambda_handler(event, context):
     print(event)
     lex = boto3.client('lex-runtime')
-#     query = event["queryStringParameters"]["q"]
-    query="show me picture of dog"
+    query = event["queryStringParameters"]["q"]
+#     query="show me picture of dog"
     lex_response = lex.post_text(
         botName='NLPControlledPhotoAlbum',
         botAlias='photoalbum',
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
     )
     print("LEX RESPONSE --- {}".format(json.dumps(lex_response)))
 
-#     query = lex_response['slots']['searchque']
+    query = lex_response['slots']['searchque']
 #    query = event['currentIntent']['slots']['searchque'] fffff
     print('query', query)
 
